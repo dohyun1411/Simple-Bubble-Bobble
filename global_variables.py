c6350a3f4fd27aaa61b27e9920ea827d239b2a36
dohyun1411 <dohyun141@kaist.ac.kr>
2021-07-22 16:04:43 +0900

    Update global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -7,6 +7,15 @@ WHITE = (255, 255, 255)
 LEFT = -1
 RIGHT = 1
 
+# Screen
+screen_width = 1200
+screen_height = 720
+screen_info = {
+    'width': screen_width,
+    'height': screen_height,
+    'offset': 50
+}
+
 # Player configs
 player_dir = RIGHT # player direction
 player_speed = 20 # player x speed
