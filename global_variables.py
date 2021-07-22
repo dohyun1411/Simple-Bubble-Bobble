e096bd4968a66de840c9222827e5406165decc78
dohyun1411 <dohyun141@kaist.ac.kr>
2021-07-22 18:20:28 +0900

    Update global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -7,16 +7,22 @@ WHITE = (255, 255, 255)
 LEFT = -1
 RIGHT = 1
 
+# Player configs
+player_dir = RIGHT # player direction
+player_speed = 15 # player x speed
+player_dx = 0 # player dx
+player_width = 64
+
+# Map configs
+brick_size = 56
+floor_interval = 220
+
 # Screen
 screen_width = 1200
 screen_height = 720
 screen_info = {
     'width': screen_width,
     'height': screen_height,
-    'offset': 50
+    'x_offset': 36,
+    'y_offset': 36 + brick_size
 }
-
-# Player configs
-player_dir = RIGHT # player direction
-player_speed = 20 # player x speed
-player_dx = 0 # player dx
