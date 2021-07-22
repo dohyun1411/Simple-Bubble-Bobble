bf745347abf7257af3d0a8f20ba6d6ec2788adb9
dohyun1411 <dohyun141@kaist.ac.kr>
2021-07-22 15:32:07 +0900

    Create global_variables.py

---
+++ global_variables.py
@@ -0,0 +1,13 @@
+import os
+
+# Colors
+WHITE = (255, 255, 255)
+
+# Directions
+LEFT = -1
+RIGHT = 1
+
+# Player configs
+player_dir = RIGHT # player direction
+player_speed = 20 # player x speed
+player_dx = 0 # player dx
