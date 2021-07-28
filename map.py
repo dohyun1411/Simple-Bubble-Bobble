4ce07418dd49a340a6a0d7a236610fd90f0214ee
dohyun1411 <dohyun141@kaist.ac.kr>
2021-07-29 04:24:11 +0900

    Updata map.py

--- map.py
+++ map.py
@@ -10,57 +10,69 @@ class Brick(pygame.sprite.Sprite):
         super(Brick, self).__init__()
         self.image = image
         self.rect = self.image.get_rect(center=pos)
+        self.pos = pos
+    
+    def get_pos(self):
+        return self.pos
+
+    def get_rect(self):
+        return self.rect
 
 floor_types = [ # _ : brick, . : empty
-    '.._________...._______',
-    '..____..____..____..__',
-    '.._____....____..___..',
-    '..____.._____...______',
-    '______....________....',
-    '_______...___...______',
-    '___..___________...___',
-    '__..___..____.._______',
-    '______..___..___..____',
-    '______________________' # only use for top and bottom
+    '________________________________',
+    '__________..........__________'
 ]
 
 def create_map(image):
 
     # 2nd and 3rd floor
-    second = random.choice(floor_types[:-1])
-    third = random.choice(floor_types[:-1])
+    second = floor_types[1]
+    third = floor_types[1]
+    fourth = floor_types[1]
 
     # bottom and top
-    bottom = random.choice(floor_types[4:])
-    if bottom == floor_types[-1]: # map is closed
-        top = random.choice(floor_types)
-    else: # map is open
-        top = bottom
+    bottom = floor_types[0]
+    top = floor_types[0]
     
     brick_group = pygame.sprite.Group()
-    # image = random.choice(list(images.values()))
-    for idx, char in enumerate(top):
+    brick_dict = {} # pos: Brick
+    # for idx, char in enumerate(top):
+    #     if char == '_':
+    #         x = idx * brick_size + brick_size // 2
+    #         y = brick_size // 2
+    #         brick_dict[(x, y)] = Brick(image, (x, y))
+
+    for idx, char in enumerate(fourth):
         if char == '_':
             x = idx * brick_size + brick_size // 2
-            y = brick_size // 2
-            brick_group.add(Brick(image, (x, y)))
+            y = fourth_y
+            brick_dict[(x, y)] = Brick(image, (x, y))
 
     for idx, char in enumerate(third):
         if char == '_':
             x = idx * brick_size + brick_size // 2
-            y = floor_interval + brick_size // 2
-            brick_group.add(Brick(image, (x, y)))
+            y = third_y
+            brick_dict[(x, y)] = Brick(image, (x, y))
 
     for idx, char in enumerate(second):
         if char == '_':
             x = idx * brick_size + brick_size // 2
-            y = 2 * floor_interval + brick_size // 2
-            brick_group.add(Brick(image, (x, y)))
+            y = second_y
+            brick_dict[(x, y)] = Brick(image, (x, y))
 
     for idx, char in enumerate(bottom):
         if char == '_':
             x = idx * brick_size + brick_size // 2
-            y = 3 * floor_interval + brick_size // 2 + 4
-            brick_group.add(Brick(image, (x, y)))
-    
-    return brick_group            
+            y = first_y
+            brick_dict[(x, y)] = Brick(image, (x, y))
+
+    for brick in brick_dict.values():
+        brick_group.add(brick)
+
+    return brick_group, brick_dict
+
+def get_nearest_brick_x(pos, brick_dict):
+    x, _ = pos
+    idx = x // brick_size
+    brick_x = idx * brick_size + brick_size // 2
+    return brick_x
