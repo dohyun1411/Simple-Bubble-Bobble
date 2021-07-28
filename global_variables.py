dba995d83af235a67f1fd47286ed01a71a285c9b
dohyun1411 <dohyun141@kaist.ac.kr>
2021-07-29 04:23:25 +0900

    Update global_variables.py

--- global_variables.py
+++ global_variables.py
@@ -2,20 +2,11 @@ import os
 
 # Colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # Directions
 LEFT = -1
 RIGHT = 1
-
-# Player configs
-player_dir = RIGHT # player direction
-player_speed = 15 # player x speed
-player_dx = 0 # player dx
-player_width = 64
-
-# Map configs
-brick_size = 56
-floor_interval = 220
 
 # Screen
 screen_width = 1200
@@ -24,5 +15,26 @@ screen_info = {
     'width': screen_width,
     'height': screen_height,
     'x_offset': 36,
-    'y_offset': 36 + brick_size
+    'y_offset': 60
 }
+fps = 60
+
+# Player configs
+player_dir = RIGHT # player direction
+player_speed = 6
+player_dx = 0
+player_dx_left = 0
+player_dx_right = 0
+player_width = 40
+player_height = 50
+is_jumpping = False
+player_jumping_speed = 20
+player_dy = 0
+gravity = 1
+
+# Map configs
+brick_size = 40
+first_y = screen_height - brick_size // 2 # 700
+second_y = first_y - 180
+third_y = second_y - 180
+fourth_y = third_y - 180
