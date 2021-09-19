cb58c55df4a5e655f33694db9a4081b5d2624a56
dohyun1411 <dohyun141@kaist.ac.kr>
2021-09-19 21:37:04 +0900

    Minor fix

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from bubble_bobble.gamelauncher import GameLauncher
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--heart", type=int, default=3)
     parser.add_argument("-f", "--fast", action="store_true")
     args = parser.parse_args()
 
